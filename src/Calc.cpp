2cadc10279b184eec271689206ea5107b8caf0a5
NoumanAmir657 <noumanamir453@gmail.com>
2024-03-02 14:27:12 +0500

    ignore and input

--- src/Calc.cpp
+++ src/Calc.cpp
@@ -5,8 +5,7 @@
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/raw_ostream.h"
 
-static llvm::cl::opt<std::string>
-Input(llvm::cl::Positional, llvm::cl::desc("<input expression>"), llvm::cl::init(""));
+static llvm::cl::opt<std::string> Input(llvm::cl::Positional, llvm::cl::desc("<input expression>"), llvm::cl::init(""));
 
 int main(int argc, const char **argv) {
     llvm::InitLLVM X(argc, argv);
